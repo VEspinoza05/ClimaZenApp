e5a737aa01cef87857f37c5ec832eb7f1e620aee
Luismateo15 <143040833+Luismateo15@users.noreply.github.com>
2025-07-15 13:54:39 -0600

    Add task card as render item to list

--- src/views/CarbonFootprintView.js
+++ src/views/CarbonFootprintView.js
@@ -1,9 +1,14 @@
 import { Text, View, StyleSheet, FlatList } from "react-native"
 import { secondTitleScreenStyle } from "../theme/Style";
 import CarbonFootprintComponent from "../components/CarbonFootprintComponent";
+import TaskCardComponent from "../components/TaskCardComponent";
 
 export default function CarbonFootprintView() {
-    const DATA = []
+    const DATA = [
+      {id: 1, title: 'Preparar cena sin carne', hour: '6:00 pm', isCompleted: false},
+      {id: 2, title: 'Usar la lavadora con carga completa xxxxxxxxxxxxxxxxxxxxx', hour: '4:00 pm', isCompleted: true},
+      {id: 3, title: 'Plantar 1 arbol en mi patio', hour: '2:00 pm', isCompleted: true},
+    ]
 
     return(
         <View style={styles.screen}>
@@ -22,7 +27,29 @@ export default function CarbonFootprintView() {
                 </View>
                 )}
 
+              renderItem={({item, index}) => (
+                <TaskCardComponent
+                  title={item.title}
+                  hour={item.hour}
+                  isCompleted={item.isCompleted}
+                  cardStyle={[
+                    styles.verticalBorders,
+                    styles.enventAndWeatherCard,
+                    (
+                      index === 0 ? styles.firstItemBorder : 
+                      index === DATA.length - 1 ? styles.lastItemBorder :
+                      styles.noTopAndBottomBorders
+                    ),
+                    index !== DATA.length - 1 ? styles.noUsePaddingBottom : undefined,
+                  ]}
+
+                  useSeparator={index === DATA.length - 1 ? false : true}
+                />
+              )}
+              
               data={DATA}
+
+              keyExtractor={item => item.id}
             />
         </View>
     )
@@ -45,5 +72,43 @@ const styles = StyleSheet.create({
     textAlign: 'left',
     marginVertical: 16,
     paddingVertical: 0,
+  },
+  verticalBorders: {
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderLeftColor: '#6a6a6a',
+    borderRightColor: '#6a6a6a',
+  },
+  firstItemBorder: {
+    borderTopWidth: 2,
+    borderBottomWidth: 0,
+    borderTopColor: '#6a6a6a',
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+  lastItemBorder: {
+    borderBottomWidth: 2,
+    borderTopWidth: 0,
+    borderBottomColor: '#6a6a6a',
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    borderBottomLeftRadius: 12,
+    borderBottomRightRadius: 12,
+    marginBottom: 16,
+  },
+  noTopAndBottomBorders: {
+    borderBottomWidth: 0,
+    borderTopWidth: 0,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+  noUsePaddingBottom: {
+    paddingTop: 12,
+    paddingBottom: 0,
+    paddingHorizontal: 12,
   },
 })
