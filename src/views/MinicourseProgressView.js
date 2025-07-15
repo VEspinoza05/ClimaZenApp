78afb45ec26917e732289e99aef06d9718876b14
Luismateo15 <143040833+Luismateo15@users.noreply.github.com>
2025-07-15 15:12:41 -0600

    Add section list with header

--- src/views/MinicourseProgressView.js
+++ src/views/MinicourseProgressView.js
@@ -1,9 +1,47 @@
-import { Text } from "react-native"
+import { Text, SectionList, View, Image, StyleSheet } from "react-native"
+import { secondTitleScreenStyle } from '../theme/Style'
 
 export default function MinicourseProgressView({navigation}) {
+    const DATA = []
+
     return(
-        <Text>
-            MinicourseProgressView
-        </Text>
+        <View style={styles.screen}>
+            <SectionList style={{paddingHorizontal: 20}}
+              ListHeaderComponent={() => (
+                <View style={styles.listHeader}> 
+                  <Image style={styles.picture} source={require('../../assets/botellas.png')} />
+                  <Text style={[secondTitleScreenStyle.secondTitleScreen, styles.homeTitleScreen]}>Curso de manualidades con botellas plásticas</Text>
+                  <Text style={styles.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
+                  <Text style={styles.paragraph}>Creado por: Juan López</Text>
+                  <Text style={styles.paragraph}>3 secciones, 8 lecciones</Text>
+                </View>
+              )}
+
+              sections={DATA}
+            />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "#ffffff" ,
+  },
+  homeTitleScreen: {
+    textAlign: 'left',
+    paddingVertical: 0,
+  },
+  paragraph: {
+    fontSize: 12,
+    fontFamily: 'OpenSans_400Regular',
+  },
+  picture: {
+    width: '100%',
+    height: 200,
+  },
+  listHeader: {
+    paddingVertical: 20,
+    gap: 8,
+  }
+})
