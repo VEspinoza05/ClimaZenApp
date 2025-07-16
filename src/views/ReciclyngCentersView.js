b3a81ddd17d40eee453c664a96b990eea218568e
Luismateo15 <143040833+Luismateo15@users.noreply.github.com>
2025-07-16 11:38:37 -0600

    Add map view

--- src/views/ReciclyngCentersView.js
+++ src/views/ReciclyngCentersView.js
@@ -1,7 +1,25 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 export default function ReciclyngCentersView() {
     return(
-        <Text>ReciclyngCentersView</Text>
+        <View style={styles.screen}>
+            <MapView
+                style={styles.map}
+            />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+  },
+  map: {
+    width: '100%',
+    flex: 1,
+  },
+})
