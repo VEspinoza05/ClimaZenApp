a6002c63bc1864ca589b4217c3442e3db671ee09
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-07-16 19:38:22 -0600

    Add date picker

--- src/views/EventAddingView.js
+++ src/views/EventAddingView.js
@@ -1,7 +1,25 @@
-import { View, StyleSheet, TextInput } from "react-native";
+import { View, StyleSheet, TextInput, Button, Text, Pressable } from "react-native";
 import { inputStyle } from "../theme/Style"
+import DateTimePicker from '@react-native-community/datetimepicker';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useState } from "react";
 
 export default function EventAddingView() {
+    const [date, setDate] = useState(new Date());
+    const [show, setShow] = useState(false);
+    const [isDateSetted, setIsDateSetted] = useState(false)
+
+    const onChange = (event, selectedDate) => {
+        setShow(false);
+
+        if (event?.type === 'dismissed' && !isDateSetted) {
+            return;
+        }
+        
+        setIsDateSetted(true);
+        setDate(selectedDate);
+    };
+
     return(
         <View style={styles.screen}>
             <TextInput 
@@ -9,6 +27,22 @@ export default function EventAddingView() {
                 placeholder="Titulo"
                 style={[inputStyle.input, styles.titleInput]}
             />
+            <Pressable style={[inputStyle.input, styles.timeInput]} onPress={() => setShow(true)} >
+                <FontAwesome name='calendar' size={24} color={isDateSetted ? 'black' : '#6a6a6a'} />
+                <Text style={[styles.pressableLabel, isDateSetted ? {color: 'black'} : undefined]}>
+                    {isDateSetted ? date.toDateString() : 'Fecha'}
+                </Text>
+            </Pressable>
+            {show && (
+                <DateTimePicker
+                    testID="dateTimePicker"
+                    value={date}
+                    mode="date"
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                />
+            )}
         </View>
     )
 }
@@ -25,4 +59,14 @@ const styles = StyleSheet.create({
     textAlignVertical: 'top',
     color: '#000000',
   },
+  pressableLabel: {
+    fontSize: 18,   
+    fontFamily: 'OpenSans_400Regular',
+    color: '#6a6a6a',
+  },
+  timeInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  }
 })
