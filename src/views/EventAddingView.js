93c586657257f29f19d68a4020c94ebfe235cb19
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-07-16 18:21:21 -0600

    Add task title input

--- src/views/EventAddingView.js
+++ src/views/EventAddingView.js
@@ -1,7 +1,28 @@
-import { Text } from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
+import { inputStyle } from "../theme/Style"
 
 export default function EventAddingView() {
     return(
-        <Text>EventAddingView</Text>
+        <View style={styles.screen}>
+            <TextInput 
+                multiline={true}
+                placeholder="Titulo"
+                style={[inputStyle.input, styles.titleInput]}
+            />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: "#ffffff" ,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+  },
+  titleInput: {
+    height: 100,
+    textAlignVertical: 'top',
+    color: '#000000',
+  },
+})
