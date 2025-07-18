fc306fbdddcb58bed53b5db941971d8a7e7fe9e2
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-07-17 18:47:31 -0600

    Add flatlist with a text input in header

--- src/views/TaskAddingView.js
+++ src/views/TaskAddingView.js
@@ -1,7 +1,38 @@
-import { Text } from "react-native"
+import { View, StyleSheet, TextInput, FlatList } from "react-native";
+import { inputStyle } from "../theme/Style"
 
 export default function TaskAddingView() {
+    const DATA = []
+    
     return(
-        <Text>TaskAddingView</Text>
+        <View style={styles.screen}>
+            <FlatList
+                ListHeaderComponent={() => (
+                    <View>
+                        <TextInput 
+                            multiline={true}
+                            placeholder="Titulo"
+                            style={[inputStyle.input, styles.titleInput]}
+                        />
+                    </View>
+                )}
+
+                data={DATA}
+            />
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+    },
+    titleInput: {
+        height: 100,
+        textAlignVertical: 'top',
+        color: '#000000',
+  },
+})
