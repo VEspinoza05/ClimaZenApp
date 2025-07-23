8763f0995859401954d0885e4437c3f2ce9704ee
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-07-23 16:37:10 -0600

    Fix keyboard overlapping in login form

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,40 +1,50 @@
 import React from 'react';
-import { View, Text, TextInput, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, TextInput, ImageBackground, StyleSheet, ScrollView, Dimensions } from 'react-native';
 import Checkbox from 'expo-checkbox'
 import CustomButton from '../components/CustomButtonComponent'
 import Link from '../components/LinkComponent'
 import { useState } from 'react';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { backgroundStyle, cardStyle, titleStyle, inputStyle, greenButtonStyle, checkboxContainerStyle, checkboxStyle, checkboxLabelStyle, linkStyle } from "../theme/Style"
 
 export default function LoginView({ navigation }) {
   const [isChecked, setChecked] = useState(false);
 
+  const { height: screenHeight } = Dimensions.get('window');
+
   return (
-    <ImageBackground
-      source={require('../../assets/forest.jpg')}
-      style={backgroundStyle.background}
+    <KeyboardAwareScrollView
+      extraScrollHeight={100}
+      enableOnAndroid={true} 
+      keyboardShouldPersistTaps='handled'
+      contentContainerStyle={{flexGrow: 1}}
     >
-      <View style={cardStyle.card}>
-        <Text style={titleStyle.title}>Iniciar Sesión</Text>
+      <ImageBackground
+        source={require('../../assets/forest.jpg')}
+        style={[backgroundStyle.background, {minHeight: screenHeight}]}
+      >
+        <View style={cardStyle.card}>
+          <Text style={titleStyle.title}>Iniciar Sesión</Text>
 
-        <TextInput placeholder="Correo electrónico" style={inputStyle.input} />
+          <TextInput placeholder="Correo electrónico" style={inputStyle.input} />
 
-        <TextInput placeholder="Contraseña" secureTextEntry style={inputStyle.input} />
+          <TextInput placeholder="Contraseña" secureTextEntry style={inputStyle.input} />
 
-        <View style={checkboxContainerStyle.checkboxContainer}>
-          <Checkbox
-            style={checkboxStyle.checkbox}
-            value={isChecked}
-            onValueChange={setChecked}
-          />
-          <Text style={checkboxLabelStyle.checkboxLabel}>Mostrar Contraseña</Text>
+          <View style={checkboxContainerStyle.checkboxContainer}>
+            <Checkbox
+              style={checkboxStyle.checkbox}
+              value={isChecked}
+              onValueChange={setChecked}
+            />
+            <Text style={checkboxLabelStyle.checkboxLabel}>Mostrar Contraseña</Text>
+          </View>
+
+          <CustomButton title="Entrar" style={greenButtonStyle.greenButton} onPress={() => navigation.navigate('Tabs')}/>
+
+          <Link style={linkStyle.link} title="¿No tienes cuenta? Registrate" />
+          
         </View>
-
-        <CustomButton title="Entrar" style={greenButtonStyle.greenButton} onPress={() => navigation.navigate('Tabs')}/>
-
-        <Link style={linkStyle.link} title="¿No tienes cuenta? Registrate" />
-        
-      </View>
-    </ImageBackground>
+      </ImageBackground>
+    </KeyboardAwareScrollView>
   );
 }
