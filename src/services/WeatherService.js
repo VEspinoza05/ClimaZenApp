3241405221212cef196f8e35bf22b3861c51ac09
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-07-24 17:32:29 -0600

    Add weather service

---
+++ src/services/WeatherService.js
@@ -0,0 +1,29 @@
+import { supabase } from '../../lib/supabase'; // tu configuración supabase
+
+const getWeatherFromSupabase = async (lat, lon) => {
+  try {
+    const { data: { session } } = await supabase.auth.getSession();
+    const accessToken = session?.access_token;
+
+    if (!accessToken) {
+      throw new Error('Usuario no autenticado');
+    }
+
+    const response = await fetch('https://ilzjhekqwxirijweyslq.functions.supabase.co/get-weather', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ lat, lon }),
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error al obtener el clima:', error);
+    return null;
+  }
+};
+
+export default getWeatherFromSupabase;
